55c75dd6392707229b6983b29e92e7375e1dbc50
vky6366 <vishwakalyanpatil@gmail.com>
2025-11-06 23:58:41 +0530

    /suggestions route fixed

--- carbon-backend/app/main.py
+++ carbon-backend/app/main.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+from dotenv import load_dotenv
+load_dotenv()
 from fastapi.middleware.cors import CORSMiddleware
 import socket
 from app.core.middleware import request_context_middleware
